406fe7a809ce714a04e529e95f1e826c52f8cbd5
DeveloperTanmay <umas81261@gmail.com>
2025-02-10 18:51:09 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+# Datatypes and Operator
+name = "Tanmay Singh"
+age = 12
+is_student = True
+weight = 43.2
+
+
+print("Name :" , name)
+print("Datatype of Name is : ", type(name))
+
+print("Age :" , age)
+print("Datatype of Age is : ", type(age))
+
+print("Is Student :" , is_student)
+print("Datatype of Is Student is : ", type(is_student))
+
+print("Weight :" , weight)
+print("Datatype of weight is : ", type(weight))
+
+
